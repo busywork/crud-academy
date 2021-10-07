033b08a10ddbe0bb06bfed7befd6be94016165e7
Brian Chung <brianchung@live.com>
2021-10-07 16:48:30 -0500

    adds webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'none',
+  entry: './client/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
