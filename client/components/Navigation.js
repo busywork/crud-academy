68942a8a2b7781f2574e591c50947d6f71aadd36
Brian Chung <brianchung@live.com>
2021-11-03 15:25:55 -0500

    updates nav styling

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <nav className="navbar fixed-top navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand">
           CRUD
@@ -15,18 +15,53 @@ export default () => {
           />
           ACADEMY
         </Link>
-        <Link to="/campuses" className="nav-link">
-          Campuses
-        </Link>
-        <Link to="/students" className="nav-link me-auto">
-          Students
-        </Link>
-        <Link to="/campuses/create" className="nav-link">
-          Add Campus
-        </Link>
-        <Link to="/students/create" className="nav-link">
-          Add Student
-        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/campuses" className="nav-link">
+                Campuses
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/students" className="nav-link">
+                Students
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <span
+                className="nav-link dropdown-toggle"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                + Add
+              </span>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link to="/campuses/create" className="dropdown-item">
+                    Campus
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/students/create" className="dropdown-item">
+                    Student
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
