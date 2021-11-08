e2e9aba25219c20c651003a40cac2c584a018f5a
Brian Chung <brianchung@live.com>
2021-11-08 12:34:39 -0600

    creates environment variables

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const db = require('./db');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
