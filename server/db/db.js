d368ab9a12f6f1710ea30c3289240a937ccfd990
Brian Chung <brianchung@live.com>
2021-11-08 16:54:26 -0600

    notes postgres config for deployment

--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,15 @@ const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`, {
+  // Postgres SSL config
+  // https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
+  // dialect: 'postgres',
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false,
+  //   },
+  // },
   logging: false,
 });
 
